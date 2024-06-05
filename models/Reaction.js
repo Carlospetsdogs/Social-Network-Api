8feace2bd933c2a9af776288c68d2b5308ff8b9c
Carlos Ruiz <carlosruiz9221@gmail.com>
2024-06-05 09:02:34 -0400

    added comments to Reaction.js

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,21 +1,28 @@
+// Import necessary modules from Mongoose
 const { Schema, Types } = require('mongoose');
+// Import the dateFormat utility function
 const dateFormat = require('../utils/dateFormat');
 
+// Define the Reaction schema
 const reactionSchema = new Schema(
     {
+        // Define the reactionId field, which is an ObjectId and has a default value
         reactionId: {
             type: Schema.Types.ObjectId,
             default: () => new Types.ObjectId()
         },
+        // Define the reactionBody field, which is a string and is required with a max length of 280
         reactionBody: {
             type: String,
             required: true,
             maxlength: 280
         },
+        // Define the username field, which is a string and is required
         username: {
             type: String,
             required: true
         },
+        // Define the createdAt field, which is a date with a default value and a getter to format the timestamp
         createdAt: {
             type: Date,
             default: Date.now,
@@ -23,11 +30,14 @@ const reactionSchema = new Schema(
         }
     },
     {
+        // Configure the schema to include getters in JSON output
         toJSON: {
             getters: true
         },
+        // Disable the virtual 'id' field
         id: false
     }
 );
 
+// Export the reactionSchema
 module.exports = reactionSchema;
