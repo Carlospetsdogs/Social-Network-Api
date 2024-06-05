9f6dd0f6836aeea96d6f6457d1322f42058720f1
Carlos Ruiz <carlosruiz9221@gmail.com>
2024-06-05 09:45:15 -0400

    added all routes and functions to thought-controller.js

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,3 +1,127 @@
-const { Thought , User } = require('../models');
+const { Thought, User } = require('../models');
 
-const thoughtController = {}
+const thoughtController = {
+  // Get all thoughts
+  async getThoughts(req, res) {
+    try {
+      const dbThoughtData = await Thought.find().sort({ createdAt: -1 });
+      res.json(dbThoughtData);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json(err);
+    }
+  },
+
+  // Get single thought by ID
+  async getSingleThought(req, res) {
+    try {
+      const dbThoughtData = await Thought.findOne({ _id: req.params.thoughtId });
+      if (!dbThoughtData) {
+        return res.status(404).json({ message: 'No thought with this ID!' });
+      }
+      res.json(dbThoughtData);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json(err);
+    }
+  },
+
+  // Create a thought
+  async createThought(req, res) {
+    try {
+      const dbThoughtData = await Thought.create(req.body);
+      const dbUserData = await User.findOneAndUpdate(
+        { _id: req.body.userId },
+        { $push: { thoughts: dbThoughtData._id } },
+        { new: true }
+      );
+      if (!dbUserData) {
+        return res.status(404).json({ message: 'Thought created but no user with this ID!' });
+      }
+      res.json({ message: 'Thought successfully created!' });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json(err);
+    }
+  },
+
+  // Update a thought
+  async updateThought(req, res) {
+    try {
+      const dbThoughtData = await Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $set: req.body },
+        { runValidators: true, new: true }
+      );
+      if (!dbThoughtData) {
+        return res.status(404).json({ message: 'No thought with this ID!' });
+      }
+      res.json(dbThoughtData);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json(err);
+    }
+  },
+
+  // Delete a thought
+  async deleteThought(req, res) {
+    try {
+      const dbThoughtData = await Thought.findOneAndRemove({ _id: req.params.thoughtId });
+      if (!dbThoughtData) {
+        return res.status(404).json({ message: 'No thought with this ID!' });
+      }
+
+      // Remove thought ID from user's `thoughts` field
+      const dbUserData = await User.findOneAndUpdate(
+        { thoughts: req.params.thoughtId },
+        { $pull: { thoughts: req.params.thoughtId } },
+        { new: true }
+      );
+      if (!dbUserData) {
+        return res.status(404).json({ message: 'Thought deleted but no user with this ID!' });
+      }
+      res.json({ message: 'Thought successfully deleted!' });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json(err);
+    }
+  },
+
+  // Add a reaction to a thought
+  async addReaction(req, res) {
+    try {
+      const dbThoughtData = await Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $addToSet: { reactions: req.body } },
+        { runValidators: true, new: true }
+      );
+      if (!dbThoughtData) {
+        return res.status(404).json({ message: 'No thought with this ID!' });
+      }
+      res.json(dbThoughtData);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json(err);
+    }
+  },
+
+  // Remove reaction from a thought
+  async removeReaction(req, res) {
+    try {
+      const dbThoughtData = await Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $pull: { reactions: { reactionId: req.params.reactionId } } },
+        { runValidators: true, new: true }
+      );
+      if (!dbThoughtData) {
+        return res.status(404).json({ message: 'No thought with this ID!' });
+      }
+      res.json(dbThoughtData);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json(err);
+    }
+  },
+};
+
+module.exports = thoughtController;
