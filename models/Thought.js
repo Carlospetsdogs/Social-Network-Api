5c347252c4bd40fa3e3289e55de1cdf4f05c6553
Carlos Ruiz <carlosruiz9221@gmail.com>
2024-06-05 08:54:56 -0400

    added more content to Thought.js

--- models/Thought.js
+++ models/Thought.js
@@ -2,3 +2,42 @@ const { Schema, model } = require('mongoose');
 const reactionSchema = require('./Reaction');
 const dateFormat = require('../utils/dateFormat');
 
+
+// Define the Thought schema
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: 'You need to leave a thought!',
+      minlength: 1,
+      maxlength: 280
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: timestamp => dateFormat(timestamp) // Apply dateFormat getter to createdAt field
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    reactions: [reactionSchema] // Array of reactions, assumed to be defined elsewhere
+  },
+  {
+    toJSON: {
+      getters: true // Enable getters to transform the response object
+    },
+    id: false // Disable the virtual 'id' field
+  }
+);
+
+// Create a virtual property `reactionCount` that retrieves the length of the reactions array
+thoughtSchema.virtual('reactionCount').get(function() {
+  return this.reactions.length;
+});
+
+// Create the Thought model using the thoughtSchema
+const Thought = model('Thought', thoughtSchema);
+
+// Export the Thought model
+module.exports = Thought;
